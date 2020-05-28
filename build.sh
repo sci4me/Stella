c94164491c51a084e6a45c96603c9c9b10bdaf70
Scitoshi Nakayobro <matthewtpeterson1@gmail.com>
2020-05-28 03:48:18 -0400

    Forgive me for I have sinned.
    However, without a _solid_ explanation of why libgcc
    is _necessary_, I can't help but feel like it's a.. eh
    Lame thing about GCC? I don't know. Do other compilers
    do the same thing?
    
    I mean, I get why I need to link with libgcc. But I don't
    really understand why they deemed it necessary to build
    software in this way. Why is it better to link whatever
    that code may or may not be as a separate library
    rather than just having the compiler generate it?
    
    *shrugs*
    
    As far as I can tell, it doesn't interfere with our
    libclessness. So. :P

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ EXECUTABLE=stella
 # Couldn't get it to "work" (define __STDC_HOSTED__ as 0)
 
 CXXFLAGS="-std=c++17 -g -nostdlib -fno-builtin -fno-rtti -fno-exceptions -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-stack-protector -Wa,--noexecstack"  # -s
-LDFLAGS="-L$VENDOR_DIR/GLEW/lib -lGLEW -lGL -lX11 -msse4.1"
+LDFLAGS="-L$VENDOR_DIR/GLEW/lib -lGLEW -lGL -lX11 -lgcc -msse4.1"
 INCLUDES="-I$VENDOR_DIR/GLEW/include -I$VENDOR_DIR/GLFW/include -I$VENDOR_DIR/imgui -I$VENDOR_DIR/sci.h -I$VENDOR_DIR/stb -I$VENDOR_DIR/rnd -I$VENDOR_DIR/pt_math"
 
 mkdir -p $BUILD_DIR
