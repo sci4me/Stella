4d5d8156ba919a68872b3bff06581ba4a1df05e3
Scitoshi Nakayobro <matthewtpeterson1@gmail.com>
2020-05-31 23:39:31 -0400

    Update TODO

--- vendor/imgui/build_static.sh
+++ vendor/imgui/build_static.sh
@@ -9,7 +9,7 @@ DEFINES="-DIMGUI_USER_CONFIG=\"stella_imconfig.hpp\""
 CXXFLAGS="-std=c++17 -g -nostdlib -fno-builtin -fno-rtti -fno-exceptions -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-stack-protector -Wa,--noexecstack"
 LDFLAGS=""
 
-INCLUDES="-I ${VENDOR_DIR}/GLEW/include -I ${VENDOR_DIR}/../src"
+INCLUDES="-I ${VENDOR_DIR}/../src"
 SOURCES="${SRC_DIR}/imgui.cpp ${SRC_DIR}/imgui_draw.cpp ${SRC_DIR}/imgui_widgets.cpp"
 OBJECTS="imgui.o imgui_draw.o imgui_widgets.o"
 
